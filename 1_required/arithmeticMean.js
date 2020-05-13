e1ede0a4be74b6fd8d9fe59f9363174156b0ce77
balashovmb <balashovmb@gmail.com>
2020-05-13 17:30:53 +0500

    Done

---
+++ 1_required/arithmeticMean.js
@@ -0,0 +1,18 @@
+let currentInput;
+let numberInputs = 0;
+let arithmeticMean = 0;
+let sum = 0;
+do {
+    currentInput = prompt('Введите число');
+    if (!isNaN(currentInput) && Boolean(currentInput)) {
+        numberInputs++;
+        sum += parseFloat(currentInput);
+        arithmeticMean = sum / numberInputs;
+    }
+} while ( currentInput );
+
+alert (
+    `Количество чисел: ${numberInputs}, ` +
+    `сумма: ${sum}, ` +
+    `среднее арифметическое: ${arithmeticMean}`
+);
---
+++ 1_required/lettersCount.js
@@ -0,0 +1,19 @@
+const keyCodeA = 'a'.charCodeAt(0);
+const keyCodeZ = 'z'.charCodeAt(0);
+const vowels = ["a", "e", "i", "o", "u", "y"];
+let vovelCount = 0;
+let consonantCount = 0;
+
+const phrase = prompt ("Введите текст").toLowerCase();
+
+[...phrase].forEach(c => {
+    if (c.charCodeAt(0) >= keyCodeA && c.charCodeAt(0) <= keyCodeZ) {
+        if (vowels.includes(c)) {
+            vovelCount ++;
+        } else {
+            consonantCount ++;
+        }
+    }
+});
+
+alert(`В тексте ${vovelCount} гласных и ${consonantCount} согласных.`);
