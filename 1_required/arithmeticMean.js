6b14c3bf4165b99cdced455c31d36b0fa8c866ff
balashovmb <balashovmb@gmail.com>
2020-05-14 16:44:08 +0500

    Fix based on comment + orthography fix

--- 1_required/arithmeticMean.js
+++ 1_required/arithmeticMean.js
@@ -4,12 +4,12 @@ let arithmeticMean = 0;
 let sum = 0;
 do {
     currentInput = prompt('Введите число');
-    if (!isNaN(currentInput) && Boolean(currentInput)) {
+    if (!isNaN(currentInput) && currentInput !== '') {
         numberInputs++;
         sum += parseFloat(currentInput);
         arithmeticMean = sum / numberInputs;
     }
-} while ( currentInput );
+} while (currentInput);
 
 alert (
     `Количество чисел: ${numberInputs}, ` +
--- 1_required/lettersCount.js
+++ 1_required/lettersCount.js
@@ -1,7 +1,7 @@
 const keyCodeA = 'a'.charCodeAt(0);
 const keyCodeZ = 'z'.charCodeAt(0);
 const vowels = ["a", "e", "i", "o", "u", "y"];
-let vovelCount = 0;
+let vowelCount = 0;
 let consonantCount = 0;
 
 const phrase = prompt ("Введите текст").toLowerCase();
@@ -9,11 +9,11 @@ const phrase = prompt ("Введите текст").toLowerCase();
 [...phrase].forEach(c => {
     if (c.charCodeAt(0) >= keyCodeA && c.charCodeAt(0) <= keyCodeZ) {
         if (vowels.includes(c)) {
-            vovelCount ++;
+            vowelCount ++;
         } else {
             consonantCount ++;
         }
     }
 });
 
-alert(`В тексте ${vovelCount} гласных и ${consonantCount} согласных.`);
+alert(`В тексте ${vowelCount} гласных и ${consonantCount} согласных.`);
