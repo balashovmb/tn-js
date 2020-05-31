4ee36bdf9f41dbf79380457f62de1f9f7fe3befd
Muxa <balashovmb@gmail.com>
2020-05-31 20:24:32 +0500

    Additional task 2 done

---
+++ 2_additional/encode-text.js
@@ -0,0 +1,35 @@
+function encodeText(text) {
+    const textArray = text.split(' ');
+    const dictionaryObj = createDictionaryObj(textArray)
+    let encodedText = [];
+    textArray.forEach(word => {
+        encodedText.push(dictionaryObj[word]['code']);
+    });
+    return { 
+        dictionary: Object.values(dictionaryObj),
+        encodedText: encodedText.toString() 
+    };
+
+    function createDictionaryObj(textArray) {
+        let existedWords = [];
+        let usedCodes = [];
+        return textArray.reduce((acc, word) => {
+            if (!existedWords.includes(word)) {
+                existedWords.push(word);
+                let code;
+                do {
+                    code = Math.random().toString(36).substr(2)
+                } while (usedCodes.includes(code));
+                usedCodes.push(code);
+                acc[word] = { word, count: 1, code };
+            }
+            else {
+                acc[word]['count']++;
+            }
+            return acc;
+        }, {});
+    }
+}
+
+text = 'Lorem ipsum ipsum dolor.'
+console.log(encodeText(text));
