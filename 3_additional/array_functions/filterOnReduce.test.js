9ef9b275e93ea60cb72e376be34493b79b578a9b
balashovmb <balashovmb@gmail.com>
2020-06-03 14:47:49 +0500

    Array functions tests done

---
+++ 3_additional/array_functions/filterOnReduce.test.js
@@ -0,0 +1,18 @@
+describe('filterOnReduce', () => {
+    it('filterOnReduce([1, 2, 3, 4, 5], item => item > 2) is ok', () => {
+        const result = filterOnReduce([1, 2, 3, 4, 5], item => item > 2);
+        assert.deepEqual(result, [3, 4, 5]);
+    });
+    it('filterOnReduce([], item => item > 2) is ok', () => {
+        const result = filterOnReduce([], item => item > 2);
+        assert.deepEqual(result, []);
+    });
+    it('throw error if first argument is not an array', () => {
+        const result = () => { filterOnReduce('', item => item > 2) };
+        assert.throw(result, TypeError, 'First arg is not an array');
+    });
+    it('throw error if second argument is not a function', () => {
+        const result = () => { filterOnReduce([1, 2, 3, 4, 5], 'string') };
+        assert.throw(result, TypeError, 'Second arg is not a function');
+    });
+})
