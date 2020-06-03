4e842bd6138bf6ae39a75d2a67f4f8cf01dea6aa
balashovmb <balashovmb@gmail.com>
2020-06-03 14:06:03 +0500

    Initial for additional task 3. New fcs revision

--- 3_additional/fcs/core/buy-ticket.js
+++ 3_additional/fcs/core/buy-ticket.js
@@ -1,56 +1,11 @@
-function makeTime(hours, minutes) {
-    const date = new Date();
-    date.setHours(hours);
-    date.setMinutes(minutes);
-    date.setSeconds(0);
-    date.setMilliseconds(0);
-    return date.getTime();
-}
-
-/**
- * @type {Object<string, Flight>} Список всех рейсов
- */
-let flights = {
-    BH118: {
-        name: 'BH118',
-        seats: 28,
-        businessSeats: 4,
-        registrationStarts: makeTime(10, 0),
-        registartionEnds: makeTime(15, 0),
-        tickets: [
-            {
-                id: 'BH118-B50',
-                flight: 'BH118',
-                fullName: 'Ivanov I. I.',
-                type: 0,
-                seat: 18,
-                buyTime: makeTime(2, 0),
-                registrationTime: null,
-            }
-        ],
-    }
-};
-
-/**
- * Добавление рейса
- * 
- * * назначение номера рейса
- * * подготовка рейса
- *   * вычисление времени регистрации
- *   * подготовка структуры Flight
- * 
- * @param {Airliner} airliner Информация о самолете
- * @param {number} time Время вылета
- * @returns {Flight}
- */
-// function createFlight(airliner, time) { }
+'use strict';
 
 /**
  * Поиск свободного места нужного типа
- * 
+ *
  * Гарантирует что найдет свободное место нужного типа или вернет null
- * 
- * @param {Flight} flight 
+ *
+ * @param {Flight} flight
  * @param {number} type
  * @returns {number} seat
  */
@@ -65,7 +20,7 @@ function findAvailableSeat(flight, type) {
 
             for (let i = flight.businessSeats + 1; i <= flight.seats; i++)
                 if (!flight.tickets.find(item => item.seat === i))
-                    availableSeats.push(i)
+                    availableSeats.push(i);
 
             if (availableSeats.length === 0)
                 return null;
@@ -87,24 +42,28 @@ function findAvailableSeat(flight, type) {
 
             return seat;
         default:
-            throw new Error(`Unknown type`)
+            throw new Error(`Unknown type`);
     }
 }
 
 /**
  * Покупка билета на самолет
- * 
+ *
  * * проверка рейса
  * * проверка возможности купить (время и наличие мест)
  * * сохранение данных билета в информации о рейсе
- * 
+ *
+ * @param {World.flights} flights Все рейсы
  * @param {string} flightName Номер рейса
  * @param {number} buyTime Время покупки
  * @param {string} fullName Имя пассажира
  * @param {number} type Тип места
- * @returns {Ticket} Возвращаем копию билета
+ * @returns { {world: World, ticket: Ticket} } Возвращаем копию билета
  */
-function buyTicket(flightName, buyTime, fullName, type = 0) {
+function buyTicket(flights, flightName, buyTime, fullName, type = 0) {
+    /**
+     * @type {Flight}
+     */
     const flight = flights[flightName];
 
     if (!flight)
@@ -120,9 +79,9 @@ function buyTicket(flightName, buyTime, fullName, type = 0) {
     if (!seat)
         throw new Error(`No seats of type ${type} available. You can choose another type`);
 
-    let id;
+    let id, exists;
     do {
-        id = flight.name + '-' + Math.random().toString().substr(2, 3);
+        id = flight.name + '-' + (flight.tickets.length + 1); // Math.random().toString().substr(2, 3);
         exists = flight.tickets.find(item => item.id === id);
     } while (exists);
 
@@ -137,33 +96,9 @@ function buyTicket(flightName, buyTime, fullName, type = 0) {
         registrationTime: null,
         type,
         seat,
-    }
+    };
 
     flight.tickets.push(ticket);
 
-    // return Object.assign({}, ticket);
-    return {
-        ...ticket,
-    };
-}
-
-const a = buyTicket('BH118', makeTime(5, 10), 'Petrov I. I.');
-console.log(a);
-
-
-function displayFlights() {
-    console.log('*** List of all flights ***');
-    console.table(flights);
-}
-
-function flightDetails(flightName) {
-    console.log(`*** Details of flight ${flightName} ***`);
-    const flight = flights[flightName];
-    if (!flight) {
-        console.warn('Flight not found');
-        return;
-    }
-
-    console.table(flight);
-    console.table(flight.tickets);
+    return {...ticket}; // копируем, что бы "пассажир" случайно ничего не изменил в самой информации о рейсе
 }
