fdc80681b249f274700f0e2e949a232d616d92d5
Muxa <balashovmb@gmail.com>
2020-06-04 20:25:50 +0500

    In process

--- 4_additional/parse-template/parse-template.test.js
+++ 4_additional/parse-template/parse-template.test.js
@@ -7,20 +7,32 @@ describe('parseTemplate', () => {
             {
                 title: 'Hello world',
                 description: 'The first program',
+                someProp: 'value'
             }
         )
         expect(testElement.innerText).not.to.equal(innerText);
+        expect(testElement.innerText).to.equal('Hello world');
     });
-
-    it('throws error when first arg is not html element', () => {
-        const result = () => parseTemplate(
-            '123',
-            {
-                title: 'Hello world',
-                description: 'The first program',
-            }
-        )
-        expect(result).to.throw(TypeError, 'First arg is not an HTTPElement');
+    describe('throws error when', () => {
+        it('first arg is not html element', () => {
+            const result = () => parseTemplate(
+                '123',
+                {
+                    title: 'Hello world',
+                    description: 'The first program',
+                }
+            )
+            expect(result).to.throw(TypeError, 'First arg is not an HTTPElement');
+        });
+        it('throws error when second arg don\'t have property', () => {
+            const result = () => parseTemplate(
+                document.getElementById('item1'),
+                {
+                    title: 'Hello world',
+                }
+            )
+            expect(result).to.throw(Error, 'Value of description is not exists');
+        });
     });
 
 })
