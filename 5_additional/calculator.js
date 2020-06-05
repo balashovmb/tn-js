35124bc7a41e7aaf467ee01c975185f33b3549c3
balashovmb <balashovmb@gmail.com>
2020-06-05 17:45:23 +0500

    5 add in process

--- 5_additional/calculator.js
+++ 5_additional/calculator.js
@@ -2,14 +2,42 @@ function calculator() {
     const firstOperand = document.getElementById('first-operand');
     const secondOperand = document.getElementById('second-operand');
     const calculateButton = document.getElementById('calculate-button');
+    const operationButtons = document.getElementsByClassName('operation-button');
+    const operationDiv = document.getElementById('operation');
 
     function calculate() {
         const firstOperandValue = parseFloat(firstOperand.value);
         const secondOperandValue = parseFloat(secondOperand.value);
-        firstOperand.value = firstOperandValue * secondOperandValue;
+        const operation = operationDiv.innerText;
+        // firstOperand.value = firstOperandValue * secondOperandValue;
+        secondOperand.value = '0';
+        switch (operation) {
+            case '+':
+                firstOperand.value = firstOperandValue + secondOperandValue;
+                break;
+            case '-':
+                firstOperand.value = firstOperandValue - secondOperandValue;
+                break;
+            case '*':
+                firstOperand.value = firstOperandValue * secondOperandValue;
+                break;
+            case '/':
+                firstOperand.value = firstOperandValue / secondOperandValue;
+                break;
+        }
+        operationDiv.innerHTML = '<br>';
     }
 
-    calculateButton.addEventListener('click',calculate);
+
+    function setOperation(event) {
+        operationDiv.innerText = event.target.innerText;
+    }
+
+    Array.from(operationButtons).forEach(function (element) {
+        element.addEventListener('click', setOperation);
+    });
+
+    calculateButton.addEventListener('click', calculate);
 }
 
 calculator();
