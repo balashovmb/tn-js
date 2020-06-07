6ce0b0a68321e547c5aef415f7c7c5f4baf3da9c
Muxa <balashovmb@gmail.com>
2020-06-07 19:42:01 +0500

    Calc in process

--- 5_additional/calculator.js
+++ 5_additional/calculator.js
@@ -4,12 +4,13 @@ function calculator() {
     const calculateButton = document.getElementById('calculate-button');
     const operationButtons = document.getElementsByClassName('operation-button');
     const operationDiv = document.getElementById('operation');
+    const cancelButton = document.getElementById('cancel-button');
+    const digitButtons = document.getElementsByClassName('digit-button');
 
     function calculate() {
         const firstOperandValue = parseFloat(firstOperand.value);
-        const secondOperandValue = parseFloat(secondOperand.value);
+        const secondOperandValue = parseFloat(secondOperand.value) || 0;
         const operation = operationDiv.innerText;
-        // firstOperand.value = firstOperandValue * secondOperandValue;
         secondOperand.value = '0';
         switch (operation) {
             case '+':
@@ -31,13 +32,36 @@ function calculator() {
 
     function setOperation(event) {
         operationDiv.innerText = event.target.innerText;
+        if (!firstOperand.value) {
+            firstOperand.value = secondOperand.value;
+            secondOperand.value = 0;
+        }
+    }
+
+    function cancel() {
+        firstOperand.value = '';
+        secondOperand.value = 0;
+    }
+
+    function inputDigit(event) {
+        const digit = event.target.innerText;
+        if (secondOperand.value == '0') {
+            secondOperand.value = digit;
+        } else {
+            secondOperand.value += digit;
+        }
     }
 
     Array.from(operationButtons).forEach(function (element) {
         element.addEventListener('click', setOperation);
     });
 
+    Array.from(digitButtons).forEach(function (element) {
+        element.addEventListener('click', inputDigit);
+    });
+
     calculateButton.addEventListener('click', calculate);
+    cancelButton.addEventListener('click', cancel);
 }
 
 calculator();
