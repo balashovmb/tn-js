cec25d5ce0fd39355d4ddfdceb74582b401c9657
balashovmb <balashovmb@gmail.com>
2020-06-09 11:43:25 +0500

    Lesson done

---
+++ 6_additional/university/core/lesson.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+* @param {Group} group Группа
+* @param {number} date дата
+* @returns {Lesson} Занятие
+ */
+
+function Lesson(date, group) {
+    this.date = date;
+    this.group = group;
+    let _absentStudents = group.whoIsAbsent();
+    this.absentStudents = function () {
+        return _absentStudents;
+    }
+    let _presentStudents = this.group.students().filter(student => !student.isIll());
+
+    this.presentStudents = () => (_presentStudents);
+
+    this.presentStudentsCount = _absentStudents.length;
+}
