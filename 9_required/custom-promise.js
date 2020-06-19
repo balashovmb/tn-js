6f2e9668040f73d0dbba8516715f9aca9d3108db
balashovmb <balashovmb@gmail.com>
2020-06-19 17:44:34 +0500

    Experiments

--- 9_required/custom-promise.js
+++ 9_required/custom-promise.js
@@ -2,27 +2,41 @@ const CustomPromise = function (callback) {
     this.__success__ = [];
     this.__error__ = [];
     this._callback = callback;
+    let state = 'pending';
+
+    this.state = function () {
+        return state;
+    }
 
     this.then = function (successCb, rejectCb) {
+        if (state !== 'pending'){
+            // this.__s__ = [successCb]
+            // this __e__ = rejectCb
+            //fire promise
+        }
         if (successCb) {
             this.__success__.push(successCb);
         }
         if (rejectCb) {
             this.__error__.push(rejectCb);
         }
+
     }
     this.catch = function (rejectCb) {
-        this.then(null, rejectCB)
+        this.then(null, rejectCb)
     }
 
     this._resolve = function (result) {
         this.__success__.forEach(cb => cb(result));
+        state = 'fulfilled';
     }
 
     this._reject = function (err) {
         this.__error__.forEach(cb => cb(err))
+        state = 'rejected';
     }
 
+    // fire promise
     setTimeout(() => {
         callback(this._resolve.bind(this), this._reject.bind(this));
     }, 0)
--- 9_required/script.js
+++ 9_required/script.js
@@ -1,12 +1,25 @@
 let i = 3;
 
-const prom = new CustomPromise((resolve, reject)=>{
-    if(i < 0 ){
-        reject(0);
-    } else {
-        resolve(1);
-    }
+// const prom = new CustomPromise(function f1 (resolve1, reject1) {
+//     setTimeout(() => {
+//         return reject1(100);
+//     }, 2000)
+// })
+// prom.then(console.log, console.error)
+// prom.then(a => (console.log(a*2) ), a => (console.error(a+1)))
+// prom.then(a => (console.log(a*3) ), console.error)
+// prom.catch(a => console.error(a-5))
+// console.log(prom);
+// console.log('realtime')
+
+const trueProm = new Promise((resolve2, reject2) => {
+
+    setTimeout(() => {
+        return reject2(100);
+    }, 2000)
 })
-prom.then(console.log, console.error)
-console.log(prom);
-console.log('realtime')
+console.log(trueProm)
+trueProm.then(a => console.log(a*a))
+trueProm.then(a => console.log(a*a*3))
+trueProm.then(trueProm);
+console.log(trueProm)
