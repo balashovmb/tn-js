a201fe7f9f662a83819bc27bd7e9c0c6a31558e7
balashovmb <balashovmb@gmail.com>
2020-06-23 10:19:59 +0500

    Lessons example

--- 10_required/giphy-search.js
+++ 10_required/giphy-search.js
@@ -1,7 +1,9 @@
+const API_KEY = "pjxDakDHEhvolGGtK4DdAGOU0KvCtERA"
+
 const getGifs = query => {
     return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
-        xhr.open('GET', `https://api.giphy.com/v1/gif/search?q=${query}`);
+        xhr.open('GET', `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`);
         xhr.onload = function () {
             if (xhr.status !== 200) {
                 reject(xhr.status);
@@ -12,3 +14,10 @@ const getGifs = query => {
         xhr.send();
     });
 };
+
+getGifs('cat').then(value => {
+    console.log(value)
+    })
+    .catch(err => {
+        console.error('Something happened', err);
+    });
