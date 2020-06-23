7370f9593c02f8842c6852ebfb3a8235f5c811da
balashovmb <balashovmb@gmail.com>
2020-06-23 14:40:31 +0500

    Required task for lesson 10 done

--- 10_required/giphy-search.js
+++ 10_required/giphy-search.js
@@ -1,36 +1,29 @@
-
 const URL = 'https://api.giphy.com/v1/gifs/search';
 const API_KEY = "pjxDakDHEhvolGGtK4DdAGOU0KvCtERA";
-let globalres;
-function search(url, api_key) {
 
+function giphySearch(url, api_key) {
     const queryForm = document.getElementById('search-form');
+    const gifsRoot = document.getElementById('gifs-root');
     let searchTimeout;
     let cache = {};
-    const gifsRoot = document.getElementById('gifs-root');
 
     const getGifs = query => {
         if (Object.keys(cache).includes(query)) {
-            console.info('from cache');
             return cache[query];
         }
         return fetch(`${url}?q=${query}&api_key=${api_key}`).then(result => {
             if (result.ok) {
-                console.info('from net');
                 const resultJSON = result.json();
                 cache[query] = resultJSON;
                 return resultJSON;
             }
             throw new Error(result.status);
-        })
+        });
     };
 
     const showResult = async (query) => {
         try {
-            console.log(query);
             const result = await getGifs(query);
-            console.log(result);
-            globalres = result;
             renderGifs(result);
         } catch (e) {
             console.error('Something happend:', e);
@@ -39,18 +32,23 @@ function search(url, api_key) {
 
     const renderGifs = function (result) {
         gifsRoot.innerHTML = '';
-        Array.from(result.data).forEach(el => {
-            const url = el.images.fixed_height.url;
-            const div = document.createElement('div');
-            div.innerHTML = `<img src=${url}/>`;
-            gifsRoot.append(div);
-        })
-    }
+        Array.from(result.data).forEach((el, index) => {
+            const imgUrl = el.images.fixed_height.url;
+            const img = new Image();
+            img.src = imgUrl;
+            gifsRoot.append(img);
+            if ((index + 1) % 4 == 0) {
+                const br = document.createElement('br');
+                gifsRoot.append(br);
+            }
+        });
+    };
 
     queryForm.addEventListener('input', function (event) {
         clearTimeout(searchTimeout);
         const query = event.target.value;
-        searchTimeout = (setTimeout(() => showResult(query), 1000));
-    })
+        searchTimeout = (setTimeout(() => showResult(query), 500));
+    });
 }
-search(url, api_key);
+
+giphySearch(URL, API_KEY);
