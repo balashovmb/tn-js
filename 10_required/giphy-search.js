72bd890e345c0f2329c7bad1fce97d510e6200f8
balashovmb <balashovmb@gmail.com>
2020-06-23 10:33:36 +0500

    Lessons example 2

--- 10_required/giphy-search.js
+++ 10_required/giphy-search.js
@@ -1,23 +1,20 @@
 const API_KEY = "pjxDakDHEhvolGGtK4DdAGOU0KvCtERA"
 
 const getGifs = query => {
-    return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`);
-        xhr.onload = function () {
-            if (xhr.status !== 200) {
-                reject(xhr.status);
-                return;
-            }
-            resolve(JSON.parse(xhr.response));
-        };
-        xhr.send();
-    });
+    return fetch(`https://api.giphy.com/v1/gifs1/search?q=${query}&api_key=${API_KEY}`).then(result =>{
+        if (result.ok){
+            return result.json()
+        }
+        throw new Error(result.status);
+    })
 };
 
-getGifs('cat').then(value => {
-    console.log(value)
-    })
-    .catch(err => {
-        console.error('Something happened', err);
-    });
+const showResult = async (query) => {
+    try {
+        const result = await getGifs(query)
+        console.log(result)
+    } catch (e) {
+        console.error('Something happend:', e);
+    }
+};
+showResult('dog');
