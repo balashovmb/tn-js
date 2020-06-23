ced65a416d61bd22e110118c3dc3c993a185ecd5
balashovmb <balashovmb@gmail.com>
2020-06-23 11:33:16 +0500

    Prototype

--- 10_required/giphy-search.js
+++ 10_required/giphy-search.js
@@ -1,20 +1,39 @@
 const API_KEY = "pjxDakDHEhvolGGtK4DdAGOU0KvCtERA"
+let query;
+const addr = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`
+function search(addr) {
+    const queryForm = document.getElementById('search-form');
+    let searchTimeout;
+    let cache = {};
 
-const getGifs = query => {
-    return fetch(`https://api.giphy.com/v1/gifs1/search?q=${query}&api_key=${API_KEY}`).then(result =>{
-        if (result.ok){
-            return result.json()
+    const getGifs = query => {
+        if (Object.keys(cache).includes(query)) {
+            return cache[query];
         }
-        throw new Error(result.status);
-    })
-};
+        return fetch(addr).then(result => {
+            if (result.ok) {
+                const resultJSON = result.json();
+                cache[query] = resultJSON;
+                return resultJSON;
+            }
+            throw new Error(result.status);
+        })
+    };
 
-const showResult = async (query) => {
-    try {
-        const result = await getGifs(query)
-        console.log(result)
-    } catch (e) {
-        console.error('Something happend:', e);
-    }
-};
-showResult('dog');
+    const showResult = async (query) => {
+        try {
+            console.log(query);
+            const result = await getGifs(query)
+            console.log(result)
+        } catch (e) {
+            console.error('Something happend:', e);
+        }
+    };
+
+    queryForm.addEventListener('input', function (event) {
+        clearTimeout(searchTimeout);
+        const query = event.target.value;
+        searchTimeout = (setTimeout(() => showResult(query), 1000));
+    })
+}
+search(addr);
