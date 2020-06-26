4ebe03b1a6e03912e7e1d0cc68c69992e5c83a9e
balashovmb <balashovmb@gmail.com>
2020-06-26 17:32:14 +0500

    Required task for lesson 11 done

---
+++ 11_required/script.js
@@ -0,0 +1,34 @@
+const login = {
+    command: 'login',
+    channel: 'channelName1',
+    username: 'username1'
+}
+
+const message = {
+    command: 'sendMessage',
+    channel: 'channelName1',
+    text: 'HI!'
+}
+
+const message2 = {
+    command: 'sendMessage',
+    channel: 'channelName1',
+    text: 'BYE!'
+}
+
+const logout = {
+    command: 'logout',
+    channel: 'channelName1',
+}
+
+const exit = {
+    command: 'exitChat'
+}
+
+setTimeout(function sendMsgs() {
+    socket.send(JSON.stringify(login));
+    socket.send(JSON.stringify(message));
+    socket.send(JSON.stringify(message2));
+    socket.send(JSON.stringify(logout));
+    socket.send(JSON.stringify(exit));
+}, 500);
